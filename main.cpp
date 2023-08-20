e27fab99d0d7ac1ca005d1d5c7c65a7d9d64e6dd
Jdelaney1991 <delaneyep91@gmail.com>
2023-08-20 17:52:23 +0000

    Final Code New
    Signed-off-by: Jdelaney1991 <delaneyep91@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,31 +1,30 @@
 #include "mbed.h"
 
-int main() {
-    // Initialize digitalIn on p13 and p16
-    DigitalIn button1(D2);  // Assuming D2 corresponds to p13
-    DigitalIn button2(D3);  // Assuming D3 corresponds to p16
-
-    // Initialize digitalOut on p23 and p25
-    DigitalOut led1(D8);    // Assuming D8 corresponds to p23
-    DigitalOut led2(D9);    // Assuming D9 corresponds to p25
-
-    while (1) {
-        // Check if button on p13 (D2) is pressed
-        if (button1) {
-            // Turn on p23 (D8) and turn off p25 (D9)
-            led1 = 1;
-            led2 = 0;
-        } else if (button2) {
-            // Check if button on p16 (D3) is pressed
-            // Turn on p25 (D9) and turn off p23 (D8)
-            led2 = 1;
-            led1 = 0;
-        } else {
-            // Turn off both p23 (D8) and p25 (D9)
-            led1 = 0;
-            led2 = 0;
-        }
+DigitalIn  input_1  (p13);
+DigitalIn  input_2  (p16);
+DigitalOut output_1 (P23);
+DigitalOut output_2 (P25);
+ 
+int main(){
+   
+ 
+while(1){ 
+ 
+    if (input_1 == 1){  //turn on p13 if pressed
+       output_1 = 1;
+       output_2 = 0;
     }
-
-    return 0;
+    else 
+        if (input_2 == 1){
+           output_2 = 1;
+           output_1 = 0;
+    }
+        else{ 
+           output_2 = 0;
+           output_1 = 0;
+ 
+           return 0;
+        }
+ 
+}
 }
